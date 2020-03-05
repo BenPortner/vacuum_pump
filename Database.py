6e299a51fcd6821d22e051b791c72d7ce9847d05
Benjamin W. Portner <benjamin.portner@bauhaus-luftfahrt.net>
2020-03-05 13:52:26 +0100

    - Database is now a class with helper functions for: - creating a session - adding entries to tables (auto id generation) - matching locations

--- Database.py
+++ Database.py
@@ -2,32 +2,73 @@ import subprocess
 import sqlalchemy as sqla
 import sqlalchemy.ext.automap
 
-def start_session(
-    database = "bonsai",
-    username = "bonsai",
-    password = "",
-    url = "localhost",
-    port = "5432",
-):
+class Database():
 
-    # start psql server
-    try:
-        r = subprocess.check_output("pg_ctl status")
-    except subprocess.CalledProcessError as e:
-        subprocess.call("pg_ctl -l logfile start")
+    current_session = None
+    tables = None
+    base_class = None
+    metadata = None
+    engine = None
 
-    # connect to database
-    db = sqla.create_engine("postgresql://"+username+":"+password+"@"+url+":"+port+"/"+database+"", echo=True)
+    def __init__(self, **kwargs):
+        self.start_session(**kwargs)
 
-    # get database metadata
-    meta = sqla.MetaData()
-    meta.reflect(db)
+    # connect to the database, information about the tables and return a ORM class for each table
+    def start_session(
+            self, database = "bonsai", username = "bonsai", password = "",
+            url = "localhost", port = "5432"
+    ):
 
-    # create one class for each table in the database
-    Base = sqlalchemy.ext.automap.automap_base(metadata=meta)
-    Base.prepare()
+        # start psql server
+        try:
+            r = subprocess.check_output("pg_ctl status")
+        except subprocess.CalledProcessError as e:
+            subprocess.call("pg_ctl -l logfile start")
 
-    # create a session
-    session = sqla.orm.sessionmaker(bind=db)()
+        # connect to database
+        engine = sqla.create_engine("postgresql://"+username+":"+password+"@"+url+":"+port+"/"+database+"", echo=True)
 
-    return session, Base, Base.classes, meta, db
+        # get database metadata
+        metadata = sqla.MetaData()
+        metadata.reflect(engine)
+
+        # create one class for each table in the database
+        base_class = sqlalchemy.ext.automap.automap_base(metadata=metadata)
+        base_class.prepare()
+
+        # create a session
+        session = sqla.orm.sessionmaker(bind=engine)()
+
+        # save handles
+        self.current_session = session
+        self.tables = base_class.classes
+        self.base_class = base_class
+        self.metadata = metadata
+        self.engine = engine
+
+        return session, base_class.classes
+
+
+    # default method for adding entries to tables where the id is an integer
+    def add_entry(self, table, entry_data):
+        # auto-generate id
+        new_id = self.current_session.query(table).count() + 1
+        # add id field to argument dict
+        entry_data["id"] = new_id
+        # create table entry
+        return table(**entry_data)
+
+
+    # helper function to check which exiobase locations are already contained in the database
+    # right now rather stupid (check by matching database identifiers with exiobase country codes)
+    def match_exiobase_locations(self, df_exiobase_locations):
+        # get all locations from the database
+        db_locations = self.current_session.query(self.tables.location).all()
+        # extract their identifiers
+        db_ids = [l.identifier for l in db_locations]
+        # for each entry in exiobase, check if location code is in the identifier list
+        matches = df_exiobase_locations.index.isin(db_ids)
+        matched_locations = df_exiobase_locations[matches]
+        unmatched_locations = df_exiobase_locations[~matches]
+
+        return matched_locations, unmatched_locations, db_locations
--- import_exiobase.py
+++ import_exiobase.py
@@ -1,23 +1,8 @@
-from Database import start_session
+from Database import Database
 from mrio_common_metadata.conversion.exiobase_3_hybrid_io import convert_exiobase
 from mrio_common_metadata.utils import load_compressed_csv, load_compressed_csv_as_dataframe
 import pathlib
 import json
-
-
-# helper function to check which exiobase locations are already contained in the database
-# right now rather stupid (check by matching database identifiers with exiobase country codes)
-def match_exiobase_bonsai_locations(session, df_exiobase_locations):
-    # get all locations from the database
-    db_locations = session.query(tables.location).all()
-    # extract their identifiers
-    db_ids = [l.identifier for l in db_locations]
-    # for each entry in exiobase, check if location code is in the identifier list
-    matches = df_exiobase_locations.index.isin(db_ids)
-    matched_locations = df_exiobase_locations[matches]
-    unmatched_locations = df_exiobase_locations[~matches]
-
-    return matched_locations, unmatched_locations, db_locations
 
 
 
@@ -34,14 +19,15 @@ file = pathlib.Path(converted_dir / "datapackage.json")
 exiobase_metadata = json.load(open(file, "r"))
 
 # connect to the database
-session, base_class, tables, db_meta_data, db = start_session()
-
+db = Database(username = "bonsai")
+session = db.current_session
+tables = db.tables
 
 # check which exiobase locations are not yet contained in the database
-table_metadata = [r for r in exiobase_metadata["resources"] if r["name"] == "locations"][0]
-file = pathlib.Path(converted_dir / table_metadata["path"])
+exiobase_location_metadata = [r for r in exiobase_metadata["resources"] if r["name"] == "locations"][0]
+file = pathlib.Path(converted_dir / exiobase_location_metadata["path"])
 df_exiobase_locations = load_compressed_csv_as_dataframe(file, exiobase_metadata)
-matched_locations, unmatched_locations, db_locations = match_exiobase_bonsai_locations(session, df_exiobase_locations)
+matched_locations, unmatched_locations, db_locations = db.match_exiobase_locations(df_exiobase_locations)
 
 # problem with 'Wallis and Futuna Is.'
 
@@ -54,9 +40,7 @@ exiobase_license = session.query(tables.license).filter(
 exiobase_source = exiobase_metadata["sources"][0].__str__().replace('\'', '').replace('}', '').replace('{', '')
 
 # add exiobase to the database as "datasource"
-number_sources_in_db = session.query(tables.datasource).count()
-source = tables.datasource(
-    id = number_sources_in_db+1,
+source_data = dict(
     label =exiobase_metadata["name"] + " converted using mrio_common_metadata",
     license = exiobase_license,
     license_id = exiobase_license.id,
@@ -66,19 +50,23 @@ source = tables.datasource(
     version = exiobase_metadata["version"],
     description = exiobase_metadata["description"],
 )
+source_entry = db.add_entry(tables.datasource, source_data)
 
-# add exiobase contributors to the database as "agents"
-number_agents_in_db = session.query(tables.agent).count()
-for a in exiobase_metadata["contributors"]:
-    agent = tables.agent(
-        id = number_agents_in_db+1,
-        label = a["title"],
+# add two dummy agents
+agent_entries = []
+for i in range(1):
+    agent_data = dict(
+        label = "dummy agent "+str(i),
         #location = "",
         #location_id = "",
-        datasource = source,
-        datasource_id = source.id,
+        datasource = source_entry,
+        datasource_id = source_entry.id,
     )
-    number_agents_in_db += 1
+    agent_entry = db.add_entry(tables.agent, *agent_data)
+    agent_entries.append(agent_entry)
+
+
+pass
 
 
 
